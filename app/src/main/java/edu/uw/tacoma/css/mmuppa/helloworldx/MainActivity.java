e862112f4a02ded88e7b60ffb4bf27f4237144cd
mmuppa <mmuppa@uw.edu>
2019-03-27 13:09:03 -0700

    Added a log message in OnCreate

--- app/src/main/java/edu/uw/tacoma/css/mmuppa/helloworldx/MainActivity.java
+++ app/src/main/java/edu/uw/tacoma/css/mmuppa/helloworldx/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "Testing commits with git");
     }
 }
