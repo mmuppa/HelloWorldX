c0b594f58d64dfa2ab731594874ca36a6f26fb0f
mmuppa <mmuppa@uw.edu>
2019-03-27 15:51:53 -0700

    Added a log message in OnResume

--- app/src/main/java/edu/uw/tacoma/css/mmuppa/helloworldx/MainActivity.java
+++ app/src/main/java/edu/uw/tacoma/css/mmuppa/helloworldx/MainActivity.java
@@ -3,6 +3,7 @@ package edu.uw.tacoma.css.mmuppa.helloworldx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i(TAG, "Testing commits with git");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "Overrode onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "Overrode onResume");
     }
 }
